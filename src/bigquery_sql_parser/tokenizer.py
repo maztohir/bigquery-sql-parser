07e068f3a2b560ce25345f9bf4a418f5e9d793b6
Muhamad Tohir <m.tohir@go-jek.com>
2020-09-01 08:58:29 +0700

    tohir | enhanced recursive keys

--- src/bigquery_sql_parser/tokenizer.py
+++ src/bigquery_sql_parser/tokenizer.py
@@ -3,24 +3,35 @@ from .utils import hash_string
 
 
 class Tokenizer:
-    def __init__(self, syntax, prefix=""):
+    def __init__(self, syntax, prefix="BQ00012_"):
         self.syntax = syntax
-        self.tokenized_syntax, self.knowledge = self._tokenize(syntax, prefix=prefix)
+        self.prefix = prefix
+        self.tokenized_syntax, self.knowledge = self._tokenize(syntax)
+
+    def translate_key(self, key, recursive=False):
+        value = self.knowledge[key]
+        if recursive:
+            sub_keys = self._find_keys(value)
+            for sub_key in sub_keys:
+                value = value.replace(
+                    sub_key, self.translate_key(sub_key, recursive=True)
+                )
+        return value
 
     def _remove_first_and_last_parenthesis(self, syntax):
         if syntax.startswith("(") and syntax.endswith(")"):
             syntax = syntax[1:-1]
         return syntax
 
-    def find_parenthesis(self, syntax):
-        FIND_PARENTHESIS = r"\w*\((?:[^()]*|\([^.]*\))*\)"
+    def _find_parenthesis(self, syntax):
+        FIND_PARENTHESIS_PATTERN = r"\w*\((?:[^()]*|\([^.]*\))*\)"
         return re.findall(
-            FIND_PARENTHESIS, self._remove_first_and_last_parenthesis(syntax)
+            FIND_PARENTHESIS_PATTERN, self._remove_first_and_last_parenthesis(syntax)
         )
 
-    def find_lowest_parenthesis(self, syntax):
+    def _find_lowest_parenthesis(self, syntax):
         lowest = []
-        matches = self.find_parenthesis(syntax)
+        matches = self._find_parenthesis(syntax)
 
         if not matches:
             lowest.append(syntax)
@@ -29,18 +40,29 @@ class Tokenizer:
             if match == syntax:
                 lowest.append(match)
             else:
-                _lowest = self.find_lowest_parenthesis(match)
+                _lowest = self._find_lowest_parenthesis(match)
                 lowest = lowest + _lowest
 
         return lowest
 
-    def _tokenize(self, syntax, prefix=""):
+    def _find_keys(self, syntax):
+        KEY_PATTERN = rf"({self.prefix}\w+)"
+        matches = re.findall(KEY_PATTERN, syntax)
+        return matches
+
+    def _translate_syntax(self, syntax):
+        keys = self._find_keys(syntax)
+        for key in keys:
+            syntax = syntax.replace(key, self.translate_key(key, recursive=True))
+        return syntax
+
+    def _tokenize(self, syntax):
         knowledge = {}
 
-        while self.find_parenthesis(syntax):
-            lowests = self.find_lowest_parenthesis(syntax)
+        while self._find_parenthesis(syntax):
+            lowests = self._find_lowest_parenthesis(syntax)
             for lowest in lowests:
-                token = prefix + hash_string(lowest)
+                token = self.prefix + hash_string(lowest)
                 knowledge[token] = lowest
                 syntax = syntax.replace(lowest, token)
 
--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -9,14 +9,14 @@ syntax = """select
 
 tokenized_syntax = """select
     name,
-    fc622415e32281f9f844a3e1cebcfefe table
+    BQ00012_fc622415e32281f9f844a3e1cebcfefe table
   from
-    84a852ab215534c9aded2f87d89537c1
+    BQ00012_84a852ab215534c9aded2f87d89537c1
 """
 
 
 def test_token():
-    tokenizer = Tokenizer(syntax)
+    tokenizer = Tokenizer(syntax, prefix="BQ00012_")
     assert tokenizer.tokenized_syntax == tokenized_syntax
 
 
@@ -24,15 +24,50 @@ nested_syntax = """select
     name,
     cast(phone as int) table
   from
-    (select * from (select * from (select * from (select * from name)) as persons) as peoples)"""
+    (select * from
+      (select * from
+        (select * from
+          (select * from name)
+        ) as persons
+      ) as peoples
+    )"""
 
 tokenized_nested_syntax = """select
     name,
     BQ00012_fc622415e32281f9f844a3e1cebcfefe table
   from
-    BQ00012_bfe1a2fe29bbe7c954ea70f1e6b5fc9f"""
+    BQ00012_79408cebe73fc48d4af93f6222881dcb"""
 
 
 def test_nested_token():
     tokenizer = Tokenizer(nested_syntax, prefix="BQ00012_")
     assert tokenizer.tokenized_syntax == tokenized_nested_syntax
+
+
+def test_translate_key():
+    tokenizer = Tokenizer(nested_syntax, prefix="BQ00012_")
+    assert (
+        tokenizer.translate_key("BQ00012_fc622415e32281f9f844a3e1cebcfefe")
+        == "cast(phone as int)"
+    )
+
+
+def test_translate_recursive_key():
+    tokenizer = Tokenizer(nested_syntax, prefix="BQ00012_")
+    assert (
+        tokenizer.translate_key(
+            "BQ00012_79408cebe73fc48d4af93f6222881dcb", recursive=True
+        )
+        == """(select * from
+      (select * from
+        (select * from
+          (select * from name)
+        ) as persons
+      ) as peoples
+    )"""
+    )
+
+
+def test_reverse_syntaxx():
+    tokenizer = Tokenizer(nested_syntax, prefix="BQ00012_")
+    assert tokenizer._translate_syntax(tokenized_nested_syntax) == nested_syntax
