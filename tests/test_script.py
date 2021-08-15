7508a6377fe09208532b76f854739f79e09da1ea
Muhamad Tohir <m.tohir@go-jek.com>
2021-08-15 15:14:28 +0700

    tohir | fix block test

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,4 +1,5 @@
 from src.bigquery_sql_parser.script import Script
+from src.bigquery_sql_parser.tokenizer import Tokenizer
 
     
 syntax_cte = """
@@ -43,7 +44,7 @@ def test_script_blocks():
 
     script = Script(syntax)
     blocks = script.blocks
-    assert blocks[0]._text == "\n    DELETE TABLE x;"
+    assert blocks[0]._text == "\n    DELETE TABLE x"
     assert blocks[2]._text == """\n    
     SELECT
         a,
@@ -55,14 +56,32 @@ def test_script_blocks():
 def test_script_ctes():
     script  = Script(syntax_cte)
     ctes = script.ctes
-    assert ctes[0].text == """(
+    assert ctes[0].text == """
         SELECT a,b,c FROM table_1
-    )"""
-    assert ctes[2].text == """(
+    """
+    assert ctes[2].text == """
         SELECT 
             a AS no,
             b AS yes,
             SUM(c) AS total 
         FROM 
             table_2
-    )"""
+    """
+
+def test_script_triple_quotes():
+    syntax = """
+    CREATE TEMP FUNCTION dt() as ((
+        '''SELECT date('dstart');'''
+    ));
+
+    SELECT 1;
+    """
+    blocks = Script(syntax).blocks
+    print(blocks[0].text)
+    assert blocks[0].text == '''
+    CREATE TEMP FUNCTION dt() as ((
+        """SELECT date('dstart');"""
+    ))'''
+    assert blocks[1].text == '''
+
+    SELECT 1'''
